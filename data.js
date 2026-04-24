a27b0e17a4d255477d5c8918b1434171ebe0d425
aldi safitra <aldisafitramtd@gmail.com>
2026-04-24 11:00:29 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 const databaseProyek = {
-    "sistem_pos microsoft": {
-        judul: "Sistem POS microsoft ",
+    "sistem_pos ducati": {
+        judul: "Sistem POS ducati ",
         keahlian: ["HTML", "CSS", "JavaScript"],
-        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtDJgDeT818QMzBrxdsIWOjbGpj3PXIOej-g&s",
-        latar: "ini adalah proyek sistem POS untuk microsoft",
-        tantangan: "ini adalah tantangan yang saya hadapi dalam proyek ini",
+        gambar: "https://e7.pngegg.com/pngimages/88/145/png-clipart-ducati-logo-ducati-multistrada-1200-logo-motorcycle-ducati-text-transport-thumbnail.png",
+        latar: "ini adalah proyek sistem POS untuk ducati",
+        tantangan: "ducati di kenal sebagai brand premium.jadi setiap pekerjaan-baik desain,produksi",
         pelajaran: "saya mendapatkan banyak pelajaran berharga dari proyek ini, seperti bagaimana mengelola waktu dengan baik dan bekerja dalam tim",
         link: ""
     },

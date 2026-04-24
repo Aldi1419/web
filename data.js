ecce582eb3ed1d86aeea136621e127b3fc5884c1
aldi safitra <aldisafitramtd@gmail.com>
2026-04-24 10:47:35 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 const databaseProyek = {
-    "sistem_pos AE": {
-        judul: "Sistem POS ",
+    "sistem_pos microsoft": {
+        judul: "Sistem POS microsoft ",
         keahlian: ["HTML", "CSS", "JavaScript"],
         gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtDJgDeT818QMzBrxdsIWOjbGpj3PXIOej-g&s",
-        latar: "ini adalah proyek sistem POS untuk merek",
+        latar: "ini adalah proyek sistem POS untuk microsoft",
         tantangan: "ini adalah tantangan yang saya hadapi dalam proyek ini",
         pelajaran: "saya mendapatkan banyak pelajaran berharga dari proyek ini, seperti bagaimana mengelola waktu dengan baik dan bekerja dalam tim",
         link: ""

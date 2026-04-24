3c16a65f41f995f310bd2d8d65445de5fdff7338
aldi safitra <aldisafitramtd@gmail.com>
2026-04-24 10:38:51 +0700

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ const databaseProyek = {
     "sistem_pos AE": {
         judul: "Sistem POS AE",
         keahlian: ["HTML", "CSS", "JavaScript"],
-        gambar: "https://static.republika.co.id/uploads/member/images/news/large_Alter_Ego_91c64515b8_1762039805.jpeg",
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaegijpxi2WJhusaZuhAjelPpZGa9BNAQNjg&s",
         latar: "ini adalah proyek sistem POS untuk toko AE",
         tantangan: "ini adalah tantangan yang saya hadapi dalam proyek ini",
         pelajaran: "saya mendapatkan banyak pelajaran berharga dari proyek ini, seperti bagaimana mengelola waktu dengan baik dan bekerja dalam tim",
